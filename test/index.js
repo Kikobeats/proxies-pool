f02e98e87e6443cd56f4bbeb22c7e5b43bb749e9
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-11 14:47:08 +0200

    fix: update method name

--- test/index.js
+++ test/index.js
@@ -7,12 +7,12 @@ const createTunnel = require('..')
 
 const proxies = [process.env.PROXY_ONE, process.env.PROXY_TWO]
 
-const { parseUri } = createTunnel
+const { parse } = createTunnel
 
 test('do a request', async t => {
   const tunnel = createTunnel(proxies)
   t.is(tunnel.index(), 0)
-  t.deepEqual(tunnel.current(), parseUri(proxies[0]))
+  t.deepEqual(tunnel.current(), parse(proxies[0]))
   const { statusCode } = await got('http://lumtest.com/echo.json', {
     agent: tunnel(),
     json: true
@@ -20,5 +20,5 @@ test('do a request', async t => {
 
   t.is(statusCode, 200)
   t.is(tunnel.index(), 1)
-  t.deepEqual(tunnel.current(), parseUri(proxies[1]))
+  t.deepEqual(tunnel.current(), parse(proxies[1]))
 })
